f12e86068062234f0b2d4ad6ad978f66bcc3d0a0
Lynked666 <lyndonerlramirez@gmail.com>
2023-09-13 00:32:14 -0700

    Adding features

--- js/navigation.js
+++ js/navigation.js
@@ -1,23 +1,21 @@
 // Global
 let menuCont = document.getElementById('menu-content');
 let genreCont = document.getElementById('genre-cont');
-let categoriesCont = document.getElementById('categories-cont');
+let categoryCont = document.getElementById('categories-cont');
 
-// Menu Side Bar
 function showMenu(){
-menuCont.classList.toggle('show-menu-content')
-categoriesCont.classList.remove('show-categories-cont');
-genreCont.classList.remove('show-genre-cont');
+    menuCont.classList.toggle('show-menu-content');
+    categoryCont.classList.remove('show-categories-cont');
+    genreCont.classList.remove('show-genre');
 }
-
-// Show Genre
-function showGenre(){
-genreCont.classList.toggle('show-genre-cont');
-categoriesCont.classList.remove('show-categories-cont');
+// showGnere()
+function showGnere(){
+    genreCont.classList.toggle('show-genre');
+    categoryCont.classList.remove('show-categories-cont');
+    menuCont.classList.remove('show-menu-content');
 }
-
-// Show Categories
-function showCategories(){
-categoriesCont.classList.toggle('show-categories-cont');
-genreCont.classList.remove('show-genre-cont');
+function showCategory(){
+    categoryCont.classList.toggle('show-categories-cont');
+    genreCont.classList.remove('show-genre');
+    menuCont.classList.remove('show-menu-content');
 }
